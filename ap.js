36dd17037b016371ca22f157ed65331956308e5b
Prasanna Kumar Gollangi <prassu158@gmail.com>
2021-05-05 12:08:10 +0530

    Collect and Add AP details

---
+++ ap.js
@@ -0,0 +1,179 @@
+var fs = require('fs');
+var striptags = require('striptags');
+var { parse } = require('node-html-parser');
+const tabletojson = require('tabletojson').Tabletojson;
+const jsonfile = require('jsonfile')
+
+
+const NodeGeocoder = require('node-geocoder');
+
+const BATCH_SIZE = 90;
+
+function fetchData(url, options) {
+    options = Object.assign({
+        useFirstRowForHeadings: true,
+        countDuplicateHeadings: true,
+        stripHtmlFromCells: false
+    }, options)
+
+
+    return tabletojson.convertUrl(
+        url,
+        options
+    )
+}
+
+function parseDistrictData(tablesAsJson) {
+    var result = [];
+    var table = tablesAsJson[0];
+    table.forEach((row, idx) => {
+        if (idx <= 1 || idx == table.length - 1) {
+            // Skip header and total row
+            return;
+        }
+        var districtName, districtId
+        if (row.District !== "District") {
+            districtName = striptags(row.District);
+            var click = parse(row.District).firstChild.getAttribute("onclick")
+            districtId = click.substr(click.lastIndexOf('=') + 1, 3)
+        }
+        var details = {
+            district: districtName,
+            districtId: districtId,
+            totalHospitals: row["No.of Hospitals"],
+            general: {
+                total: row["10"],
+                occupied: row["11"],
+                available: row["12"],
+            },
+            icu: {
+                total: row["ICU Beds"],
+                occupied: row["O2 General Beds"],
+                available: row["General Beds"],
+            },
+            o2: {
+                total: row["Ventilator"],
+                occupied: row["8"],
+                available: row["9"],
+            },
+            Ventilators: row["Ventilator"]
+        }
+        result.push(details);
+    });
+    return result;
+}
+
+function parseHospitalData(tablesAsJson) {
+    var result = [];
+    var table = tablesAsJson[0];
+    table.forEach((row, idx) => {
+        if (idx <= 1 || idx == table.length - 1) {
+            // Skip header and total row
+            return;
+        }
+        var details = {
+            name: row["Hospital"],
+            phoneNumber: row["Hospital Number"],
+            otherDetails: `Aarogyasri Empanelment status: ${row["Aarogyasri Empanelment status"]}`,
+            general: {
+                total: row["10"],
+                occupied: row["11"],
+                available: row["12"],
+            },
+            icu: {
+                total: row["ICU Beds"],
+                occupied: row["O2 General Beds"],
+                available: row["General Beds"],
+            },
+            o2: {
+                total: row["Ventilator"],
+                occupied: row["8"],
+                available: row["9"],
+            },
+            Ventilators: row["13"]
+        }
+        result.push(details);
+    });
+    return result;
+}
+
+async function getHospitalData() {
+    console.log('Fetching district wise hospital data..')
+    var allDistrictData = await fetchData("http://dashboard.covid19.ap.gov.in/ims/hospbed_reports/process.php?districtGraph=1").then(parseDistrictData)
+    var allHospitalsData = [];
+
+    for await (let districtData of allDistrictData) {
+        console.log(`Fetching hospital data of ${districtData.district} ...`);
+        var dtHospitalData = await fetchData(`http://dashboard.covid19.ap.gov.in/ims/hospbed_reports/process.php?district=${districtData.districtId}&hospdata=1`).then(parseHospitalData);
+        dtHospitalData.forEach(dt => {
+            dt.district = districtData.district;
+        });
+        console.log(`Completed fetching hospital data of ${districtData.district}.`);
+        allHospitalsData = allHospitalsData.concat(dtHospitalData);
+    }
+    return allHospitalsData;
+}
+
+async function collectHospitalData() {
+    var allHospitals = await getHospitalData()
+    jsonfile.writeFile("ap-hospitals.json", {
+        time: new Date(),
+        hospitals: allHospitals
+    });
+}
+
+async function collectionHospitalLocations() {
+
+    const options = {
+        provider: 'mapquest',
+
+        // Optional depending on the providers
+        // fetch: customFetchImplementation,
+        apiKey: 'trYQZSIHXoqZWeet3Yl8ouAfBLlK7ldD', // 'AIzaSyBXaBrugij8OryGNqGG0-gyDHCeFwgmO9E', // for Mapquest, OpenCage, Google Premier
+        formatter: null // 'gpx', 'string', ...
+    };
+
+    const geocoder = NodeGeocoder(options);
+
+    var hospitalFile = await jsonfile.readFile("ap-hospitals.json")
+    var allHospitals = hospitalFile.hospitals;
+    console.log(`Total hospitals ${allHospitals.length}`)
+    var allHospitalsWithLocation = [];
+    let index = 0;
+    for (; index < (allHospitals.length) / BATCH_SIZE;) {
+        let startIndex = index * BATCH_SIZE;
+        const batch = allHospitals.slice(startIndex, startIndex + BATCH_SIZE);
+
+        console.log(`Fetching locations of ${startIndex} to ${startIndex + BATCH_SIZE} hospitals. Size: ${batch.length}`)
+        var batchLocationQuery = batch.map(h => {
+            return {
+                address: `${h.name}, ${h.district}, Andhra Pradesh`,
+                countryCode: 'IN',
+                limit: 1
+            }
+        })
+
+        const res = await geocoder.batchGeocode(batchLocationQuery);
+        var locations = res.map(r => {
+            return r.value[0];
+        })
+        console.log(`Locations identified ${locations.length}`)
+
+        batch.forEach((b, idx) => {
+            b.location = locations[idx]
+        });
+
+        allHospitalsWithLocation = allHospitalsWithLocation.concat(batch);
+        index++
+    }
+
+    jsonfile.writeFile("ap-hospitals-locations.json", {
+        time: new Date(),
+        hospitals: allHospitalsWithLocation
+    });
+
+}
+
+// collectHospitalData()
+collectionHospitalLocations();
+
---
+++ index.js
@@ -0,0 +1,122 @@
+
+const FormData = require('form-data');
+var striptags = require('striptags');
+var { parse } = require('node-html-parser');
+
+const NodeGeocoder = require('node-geocoder');
+
+
+
+const tabletojson = require('tabletojson').Tabletojson;
+
+
+const form = new FormData();
+
+form.append('districtGraph', 1);
+
+// tabletojson.convertUrl(
+//     'http://dashboard.covid19.ap.gov.in/ims/hospbed_reports/process.php',
+//     {
+//         useFirstRowForHeadings: true,
+//         countDuplicateHeadings: true,
+//         stripHtmlFromCells: false,
+//         got: {
+//             method: 'POST',
+//             body: form
+//         }
+//     },
+//     function (tablesAsJson) {
+//         var table = tablesAsJson[0];
+//         table .forEach((row, idx) => {
+//             if (idx == table.length - 1) {
+//                 return;
+//             }
+//             if (row.District !== "District") {
+//                 console.log(row)
+//                 console.log(striptags(row.District));
+//                 var ht = parse(row.District);
+//                 var click = parse(row.District).firstChild.getAttribute("onclick")
+//                 console.log(click.substr(click.lastIndexOf('=')+1, 3))
+
+
+
+//             }
+//         });
+//     }
+// );
+
+
+// tabletojson.convertUrl(
+//     `http://dashboard.covid19.ap.gov.in/ims/hospbed_reports/process.php?district=519&hospdata=1`,
+//     {
+//         useFirstRowForHeadings: true,
+//         countDuplicateHeadings: true,
+//         stripHtmlFromCells: false
+//     },
+//     function (tablesAsJson) {
+//         var table = tablesAsJson[0];
+//         table .forEach((row, idx) => {
+//             if (idx <= 1 || idx == table.length - 1) {
+//                 // Skip header and total row
+//                 return;
+//             }
+//             // if (row.District !== "District") {
+//             //     console.log(row)
+//             //     console.log(striptags(row.District));
+//             //     var ht = parse(row.District);
+//             //     var click = parse(row.District).firstChild.getAttribute("onclick")
+//             //     console.log(click.substr(click.lastIndexOf('=')+1, 3))
+//             // }
+//             var details = {
+//                 name: "Hospital",
+//                 phoneNumber: row["Hospital Number"],
+//                 otherDetails: `Aarogyasri Empanelment status: ${row["Aarogyasri Empanelment status"]}`,
+//                 general: {
+//                     total: row["10"],
+//                     occupied: row["11"],
+//                     available: row["12"],
+//                 },
+//                 icu: {
+//                     total: row["ICU Beds"],
+//                     occupied: row["O2 General Beds"],
+//                     available: row["General Beds"],
+//                 },
+//                 o2: {
+//                     total: row["Ventilator"],
+//                     occupied: row["8"],
+//                     available: row["9"],
+//                 },
+//                 Ventilators: row["13"]
+//             }
+//             console.log(details);
+//         });
+//     }
+// );
+
+
+// const options = {
+//     provider: 'mapquest',
+
+//     // Optional depending on the providers
+//     // fetch: customFetchImplementation,
+//     apiKey: 'trYQZSIHXoqZWeet3Yl8ouAfBLlK7ldD', // 'AIzaSyBXaBrugij8OryGNqGG0-gyDHCeFwgmO9E', // for Mapquest, OpenCage, Google Premier
+//     formatter: null // 'gpx', 'string', ...
+// };
+
+// const geocoder = NodeGeocoder(options);
+
+// const res = geocoder.batchGeocode([{
+//     address: 'PVS Rammohan Hospital Srikakulam Andhra Pradesh',
+//     countryCode: 'IN',
+//     limit: 1
+// },
+// {
+//     address: 'Baggu sarojini devi hospital Srikakulam Andhra Pradesh',
+//     countryCode: 'IN',
+//     limit: 1
+// }]);
+
+// res.then(function(result) {
+//     console.log('RESULT ', result)
+// })
+// console.log(res)
